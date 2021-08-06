60550782f19cc713b522d7c4c4390ecbb9871ca1
LAM <ngofungdaniel.lam@gmail.com>
2021-08-06 14:28:02 +0800

    pretrained network

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # VGG_classifier
-VGG-based classifier
+
+VGG-based classifier trained on CXR images (RICORD-1c and RSNA Pneumonia Challenge).  The aim of the network was to classify CXRs into 2 classes (COVID-19 or non-COVID-19).  
+
+Pre-trained network has the following specs: LR=1e-3, batch size =4, Number of freeze layers = 28, num classes = 2, epochs = 13 complete epochs of training data.
+
+### Training and testing
+
+Train using train.ipynb script
+
+Test using test.ipynb script
--- models.py
+++ models.py
@@ -1,11 +1,62 @@
 import torchvision.models
 import torch
+import torch.nn as nn
 
-def get_model_vgg16(N_CLASSES, verbose=False):
-    vgg16_pt = torchvision.models.vgg16(pretrained=True)
-    num_in_ftrs = vgg16_pt.classifier[6].in_features
-    # Set output classes to N_CLASSES
-    vgg16_pt.classifier[6] = torch.nn.Linear(num_in_ftrs, N_CLASSES)
-    if verbose:
-        print(vgg16_pt)
-    return vgg16_pt
+class VGG16_pretrained(nn.Module):
+    def __init__(self, N_CLASSES, verbose):
+        super().__init__()
+        self.N_CLASSES = N_CLASSES
+        self.verbose = verbose
+        
+        vgg16_pt = torchvision.models.vgg16(pretrained=True)
+        num_in_ftrs = vgg16_pt.classifier[6].in_features
+        # Set output classes to N_CLASSES
+        vgg16_pt.classifier[6] = nn.Linear(num_in_ftrs, self.N_CLASSES)
+        
+        if self.verbose:
+            print(vgg16_pt)
+            total_num = sum(p.numel() for p in vgg16_pt.parameters())
+            trainable_num = sum(p.numel() for p in vgg16_pt.parameters() if p.requires_grad)
+            print('Parameters total: ',total_num)
+            print('Parameters trainable: ',trainable_num)
+        self.pretrained = vgg16_pt
+    def forward(self,x):
+        x = self.pretrained(x)
+        return x
+
+class VGG16_Rajaraman(nn.Module):
+    def __init__(self, N_CLASSES, max_layer_to_freeze=0, verbose=False):
+        super().__init__()
+        self.N_CLASSES=N_CLASSES
+        self.verbose = verbose
+        self.max_layer_to_freeze = max_layer_to_freeze
+        
+        # Pretrained
+        vgg16_pt = torchvision.models.vgg16(pretrained=True)
+        self.vgg16_conv = vgg16_pt.features
+        # Freeze layers
+        for layer_idx in range(self.max_layer_to_freeze):
+            for param in self.vgg16_conv[layer_idx].parameters():
+                param.requires_grad = False
+        
+        self.GAP = nn.AdaptiveAvgPool2d((1,1)) # global average pooling
+        self.dropout = nn.Dropout(p=0.5)
+        self.flatten = nn.Flatten()
+        self.classifier = nn.Linear(512, self.N_CLASSES)
+        
+        
+        model = nn.Sequential(self.vgg16_conv, self.GAP, self.classifier)
+        if verbose:
+            print(model)
+        total_num = sum(p.numel() for p in model.parameters())
+        trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print('Parameters total: ',total_num)
+        print('Parameters trainable: ',trainable_num)
+    
+    def forward(self, x):
+        x = self.vgg16_conv(x)
+        x = self.GAP(x)
+        x = self.dropout(x)
+        x = self.flatten(x)
+        x = self.classifier(x)
+        return x
